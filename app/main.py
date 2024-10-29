fc36d790d045dae92d34ec36fb23ba8dc5be1581
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-10-29 15:38:14 -0700

    refactor: :art: Removed duplicate code & changed structure
    Removed duplicate lines of code and replaced the logic with existing functions, I also organized the request functions to align with common API standards of CRUD format.

--- app/main.py
+++ app/main.py
@@ -54,23 +54,10 @@ while True:
         time.sleep(2)
 
 
-# Read == GET
-# Welcome message
+# Welcome message = Home Page
 @app.get("/")                 # ("/") -> Path Parameter
 def root():
     return {"message": "Hello Fellow Engineers, Welcome To My FastAPI"}
-
-
-# Read == GET
-# Retrieve all posts
-@app.get("/posts")   
-def get_posts():
-    query = """
-    SELECT * FROM posts
-    """
-    cursor.execute(query)
-    posts = cursor.fetchall()
-    return {"post_detail": posts}
 
 
 # Create == Post
@@ -86,27 +73,24 @@ def create_post(post: PostCreate):
 
     # Retrieve the newest post
     new_post_id = cursor.lastrowid
-    query = """
-    SELECT * FROM posts 
-    WHERE id = %s
-    """
-    cursor.execute(query, (new_post_id,))
-    new_post = cursor.fetchone()
+    new_post = get_post(new_post_id)
     return {"post_detail": new_post}
 
 
 # Read == GET
-# Retrieve the latest singular Post
-@app.get("/posts/latest")   
-def get_latest_post():
-    # TEST:// I might need to check if there is atleast a single post in the database    
+# Retrieve all posts
+@app.get("/posts")   
+def get_posts():
     query = """
-    SELECT * FROM posts 
-    ORDER BY id DESC LIMIT 1
-    """  
+    SELECT * FROM posts
+    """
     cursor.execute(query)
-    latest_post = cursor.fetchone()
-    return {"post_detail" : latest_post}
+    posts = cursor.fetchall()
+
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Posts not found")
+    return {"post_detail": posts}
 
 
 # Read == GET
@@ -127,60 +111,38 @@ def get_post(id: int):          # (id: int) -> Validation to confirm if ID can b
     return {"post_detail" : post}
 
 
-# Delete == DELETE
-# Delete one singular Post with ID
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)       # ("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT) -> 
-                                                                         # Path Parameter and default the HTTP status code to 204
-def delete_post(id: int):          # (id: int) -> Validation to confirm if ID can be automaically turned into an integer, 
-                                   # if not the built in response will send an error to the front end
-    # CHECK://Retrieve the post before deleting
+# Read == GET
+# Retrieve the latest singular Post
+@app.get("/posts/latest")   
+def get_latest_post():
     query = """
     SELECT * FROM posts 
-    WHERE id = %s
-    """
-    cursor.execute(query, (id,))
-    existing_post = cursor.fetchone()
-
-    if not existing_post:
+    ORDER BY id DESC LIMIT 1
+    """  
+    cursor.execute(query)
+    latest_post = cursor.fetchone()
+    if not latest_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} not found")
-    
-    query = """
-    DELETE FROM posts 
-    WHERE id = %s
-    """
-    cursor.execute(query, (id,))
-    connection.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+                            detail=f"Post not found")
+    return {"post_detail" : latest_post}
 
 
-# Update == PUT
-# Update one singular Post with ID
-@app.put("/posts/{id}", status_code=status.HTTP_201_CREATED)     # ("/posts/{id}", status_code=status.HTTP_201_CREATED) -> 
-                                                                 # Path Parameter and defaults the HTTP status code to 201
-def update_post(id: int, post: PostModify):
-    # CHECK://Retrieve the post before updating
+# Read == GET
+# Retrieve the latest singular Post with user ID
+@app.get("/posts/latest/user/{user_id}")   
+def get_latest_user_post(user_id: int):
     query = """
-    SELECT * FROM posts 
-    WHERE id = %s
-    """
-    cursor.execute(query, (id,))
-    existing_post = cursor.fetchone()
-    
-    if not existing_post:
+    SELECT * FROM posts
+    WHERE user_id = %s
+    ORDER BY id DESC LIMIT 1
+    """  
+    cursor.execute(query, (user_id,))
+    latest_user_post = cursor.fetchone()
+
+    if not latest_user_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} not found")
-    
-    # UPDATE:// Update post here
-    query = """
-    UPDATE posts 
-    SET title = %s, content = %s, published = %s 
-    WHERE id = %s
-    """
-    cursor.execute(query, (post.title, post.content, post.published, id,))
-    connection.commit()
-    updated_post = cursor.fetchone()
-    return {"data": updated_post}
+                            detail=f"Post with id: {user_id} not found")
+    return {"post_detail" : latest_user_post}
 
 
 # Read == GET
@@ -198,3 +160,40 @@ def get_user_posts(user_id: int):          # (user_id: int) -> Validation to con
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Posts with user_id: {user_id} not found")
     return {"post_detail" : posts}
+
+
+# Update == PUT
+# Update one singular Post with ID
+@app.put("/posts/{id}", status_code=status.HTTP_201_CREATED)     # ("/posts/{id}", status_code=status.HTTP_201_CREATED) -> 
+                                                                 # Path Parameter and defaults the HTTP status code to 201
+def update_post(id: int, post: PostModify):
+    # CHECK:// If post exists
+    get_post(id)
+    
+    query = """
+    UPDATE posts 
+    SET title = %s, content = %s, published = %s 
+    WHERE id = %s
+    """
+    cursor.execute(query, (post.title, post.content, post.published, id,))
+    connection.commit()
+    updated_post = get_post(id)
+    return {"data": updated_post}
+
+
+# Delete == DELETE
+# Delete one singular Post with ID
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)       # ("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT) -> 
+                                                                         # Path Parameter and default the HTTP status code to 204
+def delete_post(id: int):          # (id: int) -> Validation to confirm if ID can be automaically turned into an integer, 
+                                   # if not the built in response will send an error to the front end
+    # CHECK:// If post exists
+    get_post(id)
+    
+    query = """
+    DELETE FROM posts 
+    WHERE id = %s
+    """
+    cursor.execute(query, (id,))
+    connection.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
