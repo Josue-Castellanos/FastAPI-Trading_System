62e40558b414b1409f4e3a3753c4d190bdabab3c
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-08 08:39:19 -0800

    refactor: :coffin: Removed code
    Removed unsused dependencies and function to connect to databse since fastAPI does it for us when a request is made to one of our end points

--- app/database.py
+++ app/database.py
@@ -1,11 +1,8 @@
+from typing import Annotated
+from fastapi import Depends
 from sqlmodel import create_engine, Session, SQLModel, select
 from . import models
-import time
-from sqlalchemy.exc import OperationalError
 from .config import DATABASE_URL
-
-
-engine = create_engine(DATABASE_URL)
 
 
 def get_session():
@@ -13,14 +10,5 @@ def get_session():
         yield db 
 
 
-def connect_to_database():
-    while True:
-        try:
-            # Try connecting to the database
-            with engine.connect() as connection:
-                print("Connected to Database!")
-                break  # Exit loop if successful
-        except OperationalError as error:
-            print("Connection to Database failed! Retrying in 2 seconds...")
-            print("Error:", error)
-            time.sleep(2)
+engine = create_engine(DATABASE_URL)
+SessionDep = Annotated[Session, Depends(get_session)]
