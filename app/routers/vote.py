fb22824213c5e5a7f6593f341026aadfa2ac4eec
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-12 09:42:05 -0800

    feat: :zap: Improved the voting system
    Adopted modern social media functionality of liking and unliking a post similar to Instagram and Twitter, as well as upvoting and downvoting a post like reddit.

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,8 +1,8 @@
 from typing import Annotated, Optional
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from ..oauth2 import get_current_user
-from ..models import PostCreate, PostUpdate, PostPublic, VoteType, Vote
-from ..schemas import Post, User
+from ..models import VoteType
+from ..schemas import Post, User, Vote
 from ..database import select, SessionDep
 
 
@@ -11,32 +11,46 @@ router = APIRouter(
     tags = ['Vote']
 )
 
-# USE CASE:// If Vote is 'upvote' remove post from Vote
-# Check if a post is already liked by a user
-# Check what ENUM type that vote has if its found
-# If the vote.vote_type is equal to the vote_type, delete it
-# Else if the vote.vote_type is the opostite ENUM Type, update the ENUM type in Vote
-# Else insert the newly created vote with its ENUM Type
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_vote(vote: Vote, post_id: int, user_id: int, vote_type: VoteType, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
-    if vote_type not in VoteType:
-        raise HTTPException(status_code=400, detail="Invalid vote type")
+def manage_vote(vote: Vote, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
+    db_post = session.exec(select(Post).where(Post.id == vote.post_id)).first()
+    if not db_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {vote.post_id} not found")
+    if vote.vote_type not in VoteType:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Invalid vote type")
+    db_vote = session.exec(select(Vote).where(Vote.post_id==vote.post_id, Vote.user_id==current_user.id)).first()
     
-    db_vote = session.exec((Vote).where(post_id=post_id, user_id=user_id)).first()
+    # If Vote exists then user already voted on post
+    if db_vote:
+        # Change the str to VoteType Enum for delete/update
+        vote.vote_type = VoteType(vote.vote_type)
+        if db_vote.vote_type == vote.vote_type:
+            # Delete the vote
+            session.delete(db_vote)
+            session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else: 
+            # Update the vote type
+            vote_data = vote.model_dump(exclude_unset=True)
+            db_vote.sqlmodel_update(vote_data)
+            session.add(db_vote)
+            session.commit()
+            session.refresh(db_vote)
+            return db_vote
+    else:
+        # Create vote
+        new_vote = Vote(**vote.model_dump())
+        session.add(new_vote)
+        session.commit()
+        session.refresh(new_vote)
+    return new_vote
 
-    existing_vote = session.exec((Vote).filter_by(post_id=post_id, user_id=user_id)).first()
-    if existing_vote:
-        raise HTTPException(status_code=400, detail="User has already voted on this post")
 
-    vote = Vote(post_id=post_id, user_id=user_id, vote_type=vote_type)
-    
-    session.add(vote)
-    session.commit()
 
 # Another route to retrieve posts that a user has liked
 
 # Another route to retrieve posts that a user has disliked
 
-# Another route to update the Enum type of a post
-# USE CASE:// When a user clicks on a like(ON), then clicks on it again (OFF) -> like a switch
-    
