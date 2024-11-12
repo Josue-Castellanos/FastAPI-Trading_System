8eb3c59db53fbc4755d96a38f851522ecfcb2aae
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-12 09:36:27 -0800

    feat: :sparkles: Updated each endpoint to include the number of votes a post contains
    I converted my SQL query into SQLModel that LEFT JOINS two tabels to retrieve the number of votes a posts contains.

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,77 +1,164 @@
-from typing import Annotated
-from fastapi import Response, status, HTTPException, Depends, APIRouter
-from ..models import Post, PostCreate, PostUpdate, PostPublic
-from ..database import get_session, Session, select
+from typing import Annotated, Optional
+from sqlmodel import func
+from fastapi import Response, status, HTTPException, Depends, APIRouter, Query
+from ..oauth2 import get_current_user
+from ..models import PostCreate, PostUpdate, PostPublic
+from ..schemas import Post, User, Vote
+from ..database import select, SessionDep
 
 
 router = APIRouter(
-    prefix = "/posts"
+    prefix = "/posts",
+    tags = ['Posts']
 )
-SessionDep = Annotated[Session, Depends(get_session)]
 
 """
-Posts
+    **THESE ENDPOINTS ARE COMPLETE**
+    **WORK IN PROGRESS:// INCLUDE THE USERNAME OF OWNER FOR EACH POST**
 """
+# User Creates a Post
 @router.post("/", response_model=PostPublic, status_code=status.HTTP_201_CREATED)
-def create_post(post: PostCreate, session: SessionDep,):
-    db_post = Post(**post.model_dump())
-    session.add(db_post)
+def create_post(post: PostCreate, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
+    post.user_id = current_user.id
+    new_post = Post(**post.model_dump())
+    session.add(new_post)
     session.commit()
-    session.refresh(db_post)  
+    session.refresh(new_post)
+    db_post = get_post(new_post.id, session, current_user)
     return db_post
 
 
+# Get all posts in Database using pagination
+# Default: limit <= 100 & max = 100, offsett(skip) = 0, searching str = ""
 @router.get("/", response_model=list[PostPublic])   
-def get_posts(session: SessionDep,):
-    posts = session.exec(select(Post)).all()
-    
+def get_posts(session: SessionDep, current_user: Annotated[User, Depends(get_current_user)], 
+              limit: Annotated[int, Query(le=100)] = 100, offset: int = 0, search: Optional[str] = ""):
+    stmt = (select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+                    func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+                    func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes')
+                    )
+                    .join(Vote, Vote.post_id == Post.id, isouter=True)
+                    .filter(Post.title.contains(search))
+                    .group_by(Post.id)
+                    .offset(offset)
+                    .limit(limit))
+    posts = session.exec(stmt).all()
     if not posts:
-        raise HTTPException(status_code=404, detail="No posts exist")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Post not found")
     return posts
 
 
+# Get the latest singular post in Database
 @router.get("/latest", response_model=PostPublic)   
-def get_latest_post(session: SessionDep,):
-    latest_post = session.exec(select(Post).order_by(Post.id.desc()).limit(1)).first()
+def get_latest_post(session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):    
+    latest_post = session.exec(
+        select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+               func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+               func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes'))
+               .join(Vote, Vote.post_id == Post.id, isouter=True)
+               .filter(Post.user_id == current_user.id)
+               .group_by(Post.id)
+               .order_by(Post.id.desc())
+               .limit(1)).first()
 
     if not latest_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post not found")
+                            detail="Post not found")
     return latest_post
 
 
-@router.get("/{id}", response_model=PostPublic)     
-def get_post(id: int, session: SessionDep,):          
-    post = session.get(Post, id)
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} not found")
-    return post
-
-
+# Get the latest singular user post using User_ID
 @router.get("/latest/user/{user_id}", response_model=PostPublic)   
-def get_latest_user_post(user_id: int, session: SessionDep):
-    latest_user_post = session.exec(select(Post).where(Post.user_id == user_id).order_by(Post.id.desc()).limit(1)).first()
+def get_latest_user_post(user_id: int, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
+    db_user = session.get(User, user_id)
 
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User not found or does not exist")
+    latest_user_post = session.exec(
+        select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+               func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+               func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes'))
+               .join(Vote, Vote.post_id == Post.id, isouter=True)
+               .filter(Post.user_id == user_id)
+               .group_by(Post.id)
+               .order_by(Post.id.desc())
+               .limit(1)).first()
     if not latest_user_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {user_id} not found")
+                            detail=f"Post from User {user_id} not found")
     return latest_user_post
 
 
-@router.get("/user/{user_id}", response_model=list[PostPublic])     
-def get_user_posts(user_id: int, session: SessionDep,):        
-    posts = session.exec(select(Post).where(Post.user_id == user_id)).all()
+# Get all active user posts
+@router.get("/user/me", response_model=list[PostPublic])     
+def get_current_user_posts(session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):        
+    posts = session.exec(
+        select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+               func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+               func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes'))
+               .join(Vote, Vote.post_id == Post.id, isouter=True)
+               .filter(Post.user_id == current_user.id)
+               .group_by(Post.id)).all()
     if not posts:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Posts with user_id: {user_id} not found")
+                            detail="Posts from Current User not found")
     return posts
 
 
+# Get all user posts using User ID
+@router.get("/user/{user_id}", response_model=list[PostPublic])
+def get_user_posts(user_id: int, session: SessionDep, ):
+    """
+    SELECT p.user_id, p.id, p.title, p.content, p.created, p.last_modified,
+    COALESCE(SUM(v.vote_type = 'upvote'), 0) AS upvotes,
+    COALESCE(SUM(v.vote_type = 'downvote'), 0) AS downvotes
+    FROM 
+        posts p
+    LEFT JOIN 
+        votes v ON p.id = v.post_id
+    WHERE 
+        p.user_id = 2
+    GROUP BY 
+        p.id;
+    """
+    posts = session.exec(
+        select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+               func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+               func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes'))
+               .join(Vote, Vote.post_id == Post.id, isouter=True)
+               .filter(Post.user_id == user_id).group_by(Post.id)).all()
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Posts from User {user_id} not found")
+    return posts
+
+
+# Get a single post using Post ID
+@router.get("/{id}", response_model=PostPublic)     
+def get_post(id: int, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):          
+    post = session.exec(
+        select(Post.user_id, Post.id, Post.title, Post.content, Post.created, Post.last_modified,
+               func.coalesce(func.count(Vote.vote_type == 'upvote'), 0).label('upvotes'),
+               func.coalesce(func.count(Vote.vote_type == 'downvote'), 0).label('downvotes'))
+               .join(Vote, Vote.post_id == Post.id, isouter=True)
+               .filter(Post.id == id)
+               .group_by(Post.id)).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with ID {id} not found")
+    return post
+
+
+# Update a singular post
 @router.put("/{id}", response_model=PostPublic, status_code=status.HTTP_201_CREATED)    
-def update_post(id: int, post: PostUpdate, session: SessionDep):
-    db_post = get_post(id, session)
+def update_post(id: int, post: PostUpdate, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):
+    db_post = get_post(id, session, current_user)
+
+    if db_post.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
     post_data = post.model_dump(exclude_unset=True)
     db_post.sqlmodel_update(post_data)
     session.add(db_post)
@@ -80,10 +167,14 @@ def update_post(id: int, post: PostUpdate, session: SessionDep):
     return db_post
 
 
+# Delete a singular post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)       
-def delete_post(id: int, session: SessionDep):         
-    post = get_post(id, session)
+def delete_post(id: int, session: SessionDep, current_user: Annotated[User, Depends(get_current_user)]):         
+    db_post = get_post(id, session, current_user)
     
-    session.delete(post)
+    if db_post.user_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Not authorized to perform requested action")
+    session.delete(db_post)
     session.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
