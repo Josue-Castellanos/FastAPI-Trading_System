313f1abdefe514a1ccd4067d8f49bc4900d38993
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-12 09:10:20 -0800

    feat: :truck: Added a router paths
    app now includes authentication endpoints and vote endpoints in main file.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
-from typing import Annotated
-from fastapi import FastAPI, Depends
-from .database import engine, get_session, SQLModel, Session, connect_to_database
-from .routers  import post, user
+from fastapi import FastAPI
+from .database import engine, SQLModel
+from .routers  import post, user, auth, vote
 
-
-
+# SQLModel.metadata.drop_all(engine)
 SQLModel.metadata.create_all(engine)
-
-# SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
-connect_to_database()
+#connect_to_database()
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(vote.router)
 
+
+@app.get("/")
+def root():
+    return {"message": "Home page"}
 
    
