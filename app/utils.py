fe2b889eb08579dee800b32acc861303352d06db
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-12 09:30:19 -0800

    feat: :sparkles: Added a method to authenticate a user
    This method verifies the password the user inputs when logging into the system.

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,21 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash(password: str):
+
+def hash_password(password: str):
     return pwd_context.hash(password)
+
 
 def verify_password(password, hashed_password):
     return pwd_context.verify(password, hashed_password)
+
+
+def authenticate_user(userInDB, session, username: str, password: str):
+    user = session.query(userInDB).filter(userInDB.email == username).first()
+    # user = session.exec(select(userInDB).where(userInDB.email == username)).first()
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
