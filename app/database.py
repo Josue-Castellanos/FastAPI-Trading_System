4fe790d8c1661189ec4a347e8bdb61bb8bf8aa27
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-12 09:07:10 -0800

    refactor: :truck: renamed DATABASE_URL to settings.DATABASE_URL
    Used pydantic_settings package to initialize sensitive information

--- app/database.py
+++ app/database.py
@@ -2,13 +2,12 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import create_engine, Session, SQLModel, select
 from . import models
-from .config import DATABASE_URL
+from .config import settings
 
 
 def get_session():
     with Session(engine) as db:
         yield db 
 
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 SessionDep = Annotated[Session, Depends(get_session)]
