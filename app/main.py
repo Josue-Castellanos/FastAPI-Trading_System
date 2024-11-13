9a5addcaea65ed91a248179495187319af0da991
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-13 15:50:15 -0800

    feat: :lock: CORS policy
    Added FastAPI CORS middleware package to restrict certain domains from accessing my API. As of right now anything is able to access it, later on it will be restricted to solely my websites domain.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from .database import engine, SQLModel
 from .routers  import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
-# SQLModel.metadata.drop_all(engine)
-SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #connect_to_database()
 app.include_router(post.router)
