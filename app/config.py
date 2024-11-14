c8583c4513f40dba6070de6a1b731ed3de1066df
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-14 11:45:32 -0800

    perf: :lock: Removed .env import
    The app is now using Config Vars from the Heroku dashboard instead of local enviromental variables from .env file.

--- app/config.py
+++ app/config.py
@@ -1,26 +1,23 @@
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
-import os
 
-
-# Load environment variables from .env
-load_dotenv()
 
 class Settings(BaseSettings):
-    HOST: str
-    USER: str
-    DATABASE: str
-    PASSWORD: str
+    DATABASE_HOSTNAME: str
+    DATABASE_PORT: int
+    DATABASE_USERNAME: str
+    DATABASE_NAME: str
+    DATABASE_PASSWORD: str
     SECRET_KEY: str
     ALGORITHM: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    DATABASE_URL: str
 
     @property
     def DATABASE_URL(self):
-        return f"mysql+mysqlconnector://{self.USER}:{self.PASSWORD}@{self.HOST}/{self.DATABASE}"
+        return f"mysql+mysqlconnector://{self.DATABASE_USERNAME}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOSTNAME}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
     
     class Config:
-        env_file = ".env" 
+        env_file = None
 
 settings = Settings()
 
