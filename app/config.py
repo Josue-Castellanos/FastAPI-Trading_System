af44f185b1786a7da03b764fbc89cb165f619c27
Josue Castellanos <jcastellanos@horizon.csueastbay.edu>
2024-11-19 09:46:30 -0800

    feat: :card_file_box: Added Testing Settings
    Removed the database_url in the .env file and used the variables initialized in Settings to create the databse_url string, Added a Test Settings method as well to unit test the endpoints..

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
 
 
 class Settings(BaseSettings):
@@ -10,16 +11,33 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int 
-    DATABASE_URL: str
 
     @property
-    def DATABASE_URL(self):
+    def DATABASE_URL(self) -> str:
         return f"mysql+mysqlconnector://{self.DATABASE_USERNAME}:{self.DATABASE_PASSWORD}@{self.DATABASE_HOSTNAME}:{self.DATABASE_PORT}/{self.DATABASE_NAME}"
     
-    class Config:
-        env_file = None
+    model_config = SettingsConfigDict(env_file=None)
+
+
+class TestSettings(BaseSettings):
+    TEST_DATABASE_HOSTNAME: str
+    TEST_DATABASE_USERNAME: str
+    TEST_DATABASE_NAME: str
+    TEST_DATABASE_PASSWORD: str
+    SECRET_KEY: str
+    ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    
+    @property
+    def TEST_DATABASE_URL(self) -> str:
+        return f"mysql+mysqlconnector://{self.TEST_DATABASE_USERNAME}:{self.TEST_DATABASE_PASSWORD}@{self.TEST_DATABASE_HOSTNAME}/{self.TEST_DATABASE_NAME}"
+    
+    load_dotenv()
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
+test_settings = TestSettings()
 
 
 
